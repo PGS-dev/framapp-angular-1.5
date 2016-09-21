38e0d41eced8d52dce7b591add0440e3e1d3910d
PGS-SOFT\whaberek <whaberek@pgs-soft.com>
2016-09-21 14:54:09 +0200

    feature/home_page Working slidenav in home page in all devices

--- app/components/navbar/navbar.component.js
+++ app/components/navbar/navbar.component.js
@@ -1,4 +1,16 @@
+class NavbarController {
+    constructor($mdSidenav) {
+        'ngInject'
+        this.$mdSidenav = $mdSidenav;
+    }
+
+    toggleLeft() {
+        this.$mdSidenav('left').toggle();
+    }
+}
+
 const NavbarComponent = {
+    controller: NavbarController,
     template: require('./navbar.html')
 };
 
--- app/pages/main/main-page.component.js
+++ app/pages/main/main-page.component.js
@@ -24,7 +24,7 @@ class CategoryController {
             }
         });
     }
-    
+
 }
 
 const MainPageComponent = {
